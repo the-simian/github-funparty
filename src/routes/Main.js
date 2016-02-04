c83f89792edb2c5ab9d5971e8704abdd7b8b4b58
the-simian <harlinjesse@gmail.com>
2016-02-04 00:06:48 -0600

    main view created

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -5,6 +5,12 @@ import '../assets/css/flexboxgrid.css';
 import '../assets/css/font-awesome.css';
 import '../assets/css/main.css';
 
+import Row from '../components/FlexboxGrid/Row.jsx';
+import Col from '../components/FlexboxGrid/Col.jsx';
+import Box from '../components/FlexboxGrid/Box.jsx';
+import { RaisedButton } from 'material-ui';
+import { TextField } from 'material-ui';
+import TableX from '../components/MaterialUITable/TableX.jsx';
 import DemoAppBar from '../components/DemoGroup/DemoAppBar.jsx';
 
 
@@ -14,6 +20,40 @@ class Main extends Component {
         return (
             <div>
                 <DemoAppBar title="Gihub Wacky Fun Go Party" params={ this.props.params }></DemoAppBar>
+                <Row params={ this.props.params }>
+                    <Col auto={ true } params={ this.props.params }>
+                    <Box params={ this.props.params }>
+                        <form params={ this.props.params }>
+                            <Row params={ this.props.params }>
+                                <Col auto={ true } params={ this.props.params }>
+                                <Box params={ this.props.params }>
+                                    <TextField hintText="Owner Name"
+                                               style={ {    width: '100%'} }
+                                               params={ this.props.params }></TextField>
+                                </Box>
+                                </Col>
+                                <Col auto={ true } params={ this.props.params }>
+                                <Box params={ this.props.params }>
+                                    <RaisedButton label="Fetch Potato" params={ this.props.params }></RaisedButton>
+                                </Box>
+                                </Col>
+                            </Row>
+                        </form>
+                    </Box>
+                    </Col>
+                </Row>
+                <Row params={ this.props.params }>
+                    <TableX fixedHeader={ true }
+                            fixedFooter={ true }
+                            stripedRows={ true }
+                            showRowHover={ true }
+                            selectable={ false }
+                            multiSelectable={ true }
+                            enableSelectAll={ true }
+                            deselectOnClickaway={ true }
+                            height="400px"
+                            params={ this.props.params }></TableX>
+                </Row>
             </div>
             );
     }
